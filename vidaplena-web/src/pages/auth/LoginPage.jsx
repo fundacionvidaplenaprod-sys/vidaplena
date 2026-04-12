6c17d00bc5e9378a8c51ee8c904f775d4e3aed4c
Fundacion vida plena <fundacionvidaplenaprod@gmail.com>
2026-04-12 12:34:52 -0400

    modificacion del nombre por el correcto en el login

--- vidaplena-web/src/pages/auth/LoginPage.jsx
+++ vidaplena-web/src/pages/auth/LoginPage.jsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
         try {
             // 1. Login (Tu AuthContext ya maneja la lógica de pedir usuario si falta)
             await login(data);
-            
+
             // 2. Redirección directa al Dashboard
             // (Ahí el DashboardHome se encargará de repartir a Admins o Pacientes)
             console.log("✅ Login exitoso. Entrando al dashboard...");
@@ -38,7 +38,7 @@ export default function LoginPage() {
                     <div className="bg-white p-2 rounded-full w-72 h-72 mx-auto flex items-center justify-center mb-8 shadow-2xl overflow-hidden">
                         <img src={logoVidaPlena} alt="Logo" className="w-full h-full object-contain rounded-full" />
                     </div>
-                    <h1 className="text-4xl font-bold mb-4">Fundación Vida Plena</h1>
+                    <h1 className="text-4xl font-bold mb-4">Fundación V.I.D.A. Plena</h1>
                     <p className="text-vida-light text-lg">Comprometidos con la salud y el bienestar.</p>
                 </div>
             </div>
@@ -47,8 +47,8 @@ export default function LoginPage() {
             <div className="w-full md:w-1/2 flex items-center justify-center p-8 bg-white">
                 <div className="w-full max-w-md">
                     <div className="md:hidden text-center mb-8">
-                         <img src={logoVidaPlena} alt="Logo" className="h-16 w-auto mx-auto mb-4" />
-                         <h2 className="text-2xl font-bold text-vida-primary">Bienvenido</h2>
+                        <img src={logoVidaPlena} alt="Logo" className="h-16 w-auto mx-auto mb-4" />
+                        <h2 className="text-2xl font-bold text-vida-primary">Bienvenido</h2>
                     </div>
 
                     <h2 className="hidden md:block text-3xl font-bold text-vida-primary mb-2">Iniciar Sesión</h2>
