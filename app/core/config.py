fb04add29f2dff713230e2801773786ca5990b3d
Fundacion vida plena <fundacionvidaplenaprod@gmail.com>
2026-07-30 19:27:07 -0400

    cambio tiempo de vida en registro

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     # Puedes generar una buena ejecutando: openssl rand -hex 32
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
     INVENTORY_LOW_STOCK_THRESHOLD: int = 10
 
     class Config:
